d6d2b601406e3c2a9e1059b9183d460e1ac33311
prasadpalkar94 <prasadpalkar94@gmail.com>
2021-07-20 23:41:58 +0530

    Refactored:Code for Single movie detail

--- src/app/movie/movie-detail/movie-detail.component.ts
+++ src/app/movie/movie-detail/movie-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { ApiServiceService } from 'src/app/service/api-service.service';
 
 
 @Component({
@@ -10,15 +11,26 @@ import { ActivatedRoute } from '@angular/router';
 export class MovieDetailComponent implements OnInit {
   user?: { id: string; };
   
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute, private apiService:ApiServiceService) { }
   
+  movieDetails:any = [];
 
   ngOnInit(): void {
-    this.user = {
-      id: this.route.snapshot.params['id']
-    }
+    // this.user = {
+    //   id: this.route.snapshot.params['id']
+    // };
+    // this.route.params.subscribe((data:Params)=>{
+    //   this.user={
+    //     id: data['id']
+    //   }
+    // })
+    this.getMovieDetails();
   }
 
+  getMovieDetails():void{
+      this.apiService.getDataForHome().subscribe(data => {this.movieDetails = data.results;});
+      console.log(this.movieDetails);
+  }
 
 
 }
